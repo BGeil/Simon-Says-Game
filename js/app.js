f1213b8c0100f8f09f6676beb044bab92b48fa9d
BryanG <BryanG@Bryans-MacBook-Pro.local>
2019-10-04 16:10:19 -0500

    added Math.Random function in game object

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
 console.log(`Simon Says Game`);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const game = {
 
 	player1: [],
 
-	computer: [],
+	sequence: [],
 
+	ranNum: function() {
+		const randomNumber = (Math.floor(Math.random() * 4) + 1);;
+	},
+
+
+
+
+	// flashButton: function(n) {
+	// 	if (n === 1) {
+	// 		$(`.yellow`).addClass(`flash-button`).removeClass(`flash-button`);
+	// 	} else if (n === 2) {
+	// 		$(`.red`).addClass(`flash-button`).removeClass(`flash-button`);
+	// 	} else if (n === 3) {
+	// 		$(`.green`).addClass(`flash-button`).removeClass(`flash-button`);
+	// 	} else if (n === 4) {
+	// 		$(`.blue`).addClass(`flash-button`).removeClass(`flash-button`);
+	// 	}
+	// 		//n.push(this.sequence);
+	// },
+
+	// addNextColor()
+
+	// playSequence: function () {
+
+	// },
 
 
 
@@ -13,23 +52,28 @@ const game = {
 
 
 
-$(`.startGame`).on('click', () => {
+$(`.start-game-button`).on('click', () => {
 	console.log(`The start button is working`);
-
+	
 })
 
-$(`#red`).on(`click`, () => {
-	console.log('The red button is working!');
-})
-
-$(`#green`).on(`click`, () => {
-	console.log('The green button is working!');	
-})
-
-$(`#blue`).on(`click`, () => {
-	console.log(`The blue button is working!`);
-})
-
-$(`#yellow`).on(`click`, () => {
+$(`.yellow`).on(`click`, () => {
 	console.log(`The yellow button is working!`);
+	flashButton(1);
 })
+
+$(`.red`).on(`click`, () => {
+	console.log('The red button is working!');
+	flashButton(2);
+})
+
+$(`.green`).on(`click`, () => {
+	console.log('The green button is working!');
+	flashButton(3);	
+})
+
+$(`.blue`).on(`click`, () => {
+	console.log(`The blue button is working!`);
+	flashButton(4);
+})
+
