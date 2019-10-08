f0676e8be47c35831ad751677e144ce5ce485195
BryanG <BryanG@Bryans-MacBook-Pro.local>
2019-10-08 09:24:33 -0500

    fixed timeout delay on colored buttons

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ console.log(`Simon Says Game`);
 
 const game = { // Start of Game Object
 
-    player1Seq: [],
+    player1Sequence: [],
 
-    cpuSeq: [],
+    cpuSequence: [],
 
     currentLevel: 0,
 
@@ -20,10 +20,10 @@ const game = { // Start of Game Object
 
     playSequence: function() {
     	this.generateRanNum();
-        this.cpuSeq.push(this.randomNumber);
-        this.flashButton(this.cpuSeq);
+        this.cpuSequence.push(this.randomNumber);
+        this.flashButton(this.cpuSequence);
         this.nextLevel();
-        this.player1Seq = [];
+        this.player1Sequence = [];
     },
 
     // Flashes the button on screen.
@@ -32,22 +32,22 @@ const game = { // Start of Game Object
             $(`.yellow`).addClass('flash-button')
             setTimeout(() => {
                 $(`.yellow`).removeClass('flash-button')
-            }, 1000)
+            }, 175)
         } else if (n === 2) {
             $(`.red`).addClass('flash-button')
             setTimeout(() => {
                 $(`.red`).removeClass('flash-button')
-            }, 1000)
+            }, 175)
         } else if (n === 3) {
             $(`.green`).addClass('flash-button')
             setTimeout(() => {
                 $(`.green`).removeClass('flash-button')
-            }, 1000)
+            }, 175)
         } else if (n === 4) {
             $(`.blue`).addClass('flash-button')
             setTimeout(() => {
                 $(`.blue`).removeClass('flash-button')
-            }, 1000)
+            }, 175)
         }
 
     },
@@ -68,11 +68,11 @@ const game = { // Start of Game Object
         }
     },
 
-    // Checks sequences beteween cpuSeq and player1Seq
-    compareArr: function(indexOfArr) {
-        if (this.player1Seq[indexOfArr] === this.cpuSeq[indexOfArr]) {
+    // Checks sequences beteween cpuSequence and player1Sequence
+    compareSequence: function(Arr) {
+        if (this.player1Sequence[Arr] === this.cpuSequence[Arr]) {
 
-            if (this.cpuSeq.length === this.player1Seq.length) {
+            if (this.cpuSequence.length === this.player1Sequence.length) {
                 setTimeout(function() {
                     game.playSequence();
                 }, 1000);
@@ -87,7 +87,7 @@ const game = { // Start of Game Object
         console.log("You lost! The game is over!");
         this.currentLevel = 0;
         $(".current-level").text(`Current Level: ${this.currentLevel}`);
-        this.cpuSeq = [];
+        this.cpuSequence = [];
     }
 
 } // End of Game Object
@@ -104,28 +104,28 @@ $(`.start-game-button`).on('click', () => {
 // Color Buttons ---- Cod Is definitely not DRY, but should work.
 $(`.yellow`).on(`click`, () => {
     console.log(`The yellow button is working!`);
-    game.player1Seq.push(1);
+    game.player1Sequence.push(1);
     game.flashButton(1);
-    game.compareArr(game.player1Seq.length);
+    game.compareSequence(game.player1Sequence.length);
 })
 
 $(`.red`).on(`click`, () => {
     console.log('The red button is working!');
-    game.player1Seq.push(2);
+    game.player1Sequence.push(2);
     game.flashButton(2);
-    game.compareArr(game.player1Seq.length);
+    game.compareSequence(game.player1Sequence.length);
 })
 
 $(`.green`).on(`click`, () => {
     console.log('The green button is working!');
-    game.player1Seq.push(3);
+    game.player1Sequence.push(3);
     game.flashButton(3);
-    game.compareArr(game.player1Seq.length)
+    game.compareSequence(game.player1Sequence.length)
 })
 
 $(`.blue`).on(`click`, () => {
     console.log(`The blue button is working!`);
-    game.player1Seq.push(4);
+    game.player1Sequence.push(4);
     game.flashButton(4);
-    game.compareArr(game.player1Seq.length)
+    game.compareSequence(game.player1Sequence.length)
 })
